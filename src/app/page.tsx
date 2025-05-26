a7e7907f47c2ceed4e1d20ab9f8c602f45e2cf96
viktoriia.sultanovska <viktoriia.sultanovska@gmail.com>
2025-05-26 23:38:45 +0200

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+import flagsmith from "@/utils/flagsmith";
+
+export const revalidate = 0;
+
+export default async function Home() {
+  const flags = await flagsmith.getEnvironmentFlags();
+  return (
+    <main className="main">
+      <div>
+        <h2>Hello World</h2>
+        <p>new text</p>
+      </div>
+      <Link href="/about">About</Link>
+      <br />
+      {flags.isFeatureEnabled("search") && <input placeholder="Search" />}
+    </main>
+  );
+}
